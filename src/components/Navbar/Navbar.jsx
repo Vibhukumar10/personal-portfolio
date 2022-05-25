c7b309680263729b4bd36498f751fdb910d6605c
Vibhu <vibhukumar1224@gmail.com>
2022-05-25 18:46:18 +0530

    added socials in navbar menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { HiMenuAlt4, HiX } from 'react-icons/hi'
 import { motion } from 'framer-motion'
 
+import { BsTwitter } from 'react-icons/bs'
+import { AiOutlineGithub } from 'react-icons/ai'
+import { IoLogoLinkedin } from 'react-icons/io5'
+
 import { images } from '../../constants'
 import './Navbar.scss'
 
@@ -24,22 +28,48 @@ const Navbar = () => {
 
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
-
         {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}>
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
+          <>
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}>
+              <span className="span-flex">
+                <span className="span-items-container">
+                  <a
+                    className="span-items"
+                    href="https://www.linkedin.com/in/vibhu-kumar-singh/"
+                    rel="noreferrer"
+                    target="_blank">
+                    <IoLogoLinkedin />
                   </a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
+                  <a
+                    className="span-items"
+                    href="https://github.com/Vibhukumar10"
+                    rel="noreferrer"
+                    target="_blank">
+                    <AiOutlineGithub />
+                  </a>
+                  <a
+                    className="span-items"
+                    href="https://twitter.com/VibhuKumarSing3"
+                    rel="noreferrer"
+                    target="_blank">
+                    <BsTwitter />
+                  </a>
+                </span>
+                <HiX onClick={() => setToggle(false)} />
+              </span>
+              <ul>
+                {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          </>
         )}
       </div>
     </nav>
