32bb860e50d8540142cb9a4e4737a2abff91b929
Vibhu <vibhukumar1224@gmail.com>
2022-05-25 18:10:25 +0530

    links updated

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,17 +6,29 @@ import { IoLogoLinkedin } from 'react-icons/io5'
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <a href="https://www.linkedin.com/in/vibhu-kumar-singh/" style={{ paddingTop: '4px' }}>
+      <a
+        href="https://www.linkedin.com/in/vibhu-kumar-singh/"
+        style={{ paddingTop: '4px' }}
+        rel="noreferrer"
+        target="_blank">
         <IoLogoLinkedin />
       </a>
     </div>
     <div>
-      <a href="https://github.com/Vibhukumar10" style={{ paddingTop: '4px' }}>
+      <a
+        href="https://github.com/Vibhukumar10"
+        style={{ paddingTop: '4px' }}
+        rel="noreferrer"
+        target="_blank">
         <AiOutlineGithub />
       </a>
     </div>
     <div>
-      <a href="https://twitter.com/VibhuKumarSing3" style={{ paddingTop: '4px' }}>
+      <a
+        href="https://twitter.com/VibhuKumarSing3"
+        style={{ paddingTop: '4px' }}
+        rel="noreferrer"
+        target="_blank">
         <BsTwitter />
       </a>
     </div>
